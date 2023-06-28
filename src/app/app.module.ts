fb4d806dbaf0d31297ec4c1352b13e0617481437
Melek Soyhan <melek.soyhan5@gmail.com>
2023-06-28 08:54:30 +0200

    Ein Kommentar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,6 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // Komponente mit der die Anwendung starten soll.
 })
 export class AppModule { }
